33441b7326b717c3219e44d0628639fc70c3e472
yujin032497 <49347652+yujin032497@users.noreply.github.com>
2022-02-02 23:04:46 +0900

    Update 10991.py

--- 10991.py
+++ 10991.py
@@ -1,21 +1,21 @@
 # 2022-02-02
 # 별 찍기 - 16
-N=int(input())
+N=int(input()) # 찍을 별 줄 수
 for i in range(0,N):
     s=''
     for j in range(1,2*N):
         if j>=(N-i) and j<=(N+i):
-            if N%2==0:
-                if i%2==0 and j%2==0:
+            if N%2==0: # 찍을 별 줄 수 가 짝수 일 때,
+                if i%2==0 and j%2==0: # 행,열이 짝수인 경우
                     s+='*'
-                elif i%2!=0 and j%2!=0:
+                elif i%2!=0 and j%2!=0: # 행,열이 홀수인 경우
                     s+='*'
                 else:
                     s+=' '
-            else:
-                if i%2==0 and j%2!=0:
+            else: # 찍을 별 줄 수 가 홀수 일 때,
+                if i%2==0 and j%2!=0: # 행이 짝수이고 열이 홀수 일 때
                     s+='*'
-                elif i%2!=0 and j%2==0:
+                elif i%2!=0 and j%2==0: #행이 홀수이고 열이 짝수 일 때
                     s+='*'
                 else:
                     s+=' '
