097b03c4f187c0fd64564119b3349b6f0276b931
yujin032497 <49347652+yujin032497@users.noreply.github.com>
2022-02-17 22:58:41 +0900

    Create 2161.py

---
+++ 2161.py
@@ -0,0 +1,12 @@
+# 2022-02-17
+# 카드 1
+import sys
+M=[]
+N=int(sys.stdin.readline()) # 카드 N장
+C=[x for x in range(1,N+1)] # 1 ~ N까지의 카드 리스트
+while len(C)>1: # 1장 남을때까지
+    print(C[0]) # 버리는 카드
+    del C[0] # 버리고
+    C.append(C[0]) # 넘기고
+    del C[0] # 넘긴카드 버리기
+print(C[0]) # 마지막 남은 카드 
